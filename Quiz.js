a6f41f60311d204fc27a38a3c29d928330ccdbcf
mayursakthivel <38622067+mayursakthivel@users.noreply.github.com>
2019-06-03 05:02:53 -0400

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,19 @@
+import React , {Component} from 'react'
+
+
+let quizData = require('./quiz-data.json')
+class Quiz extends Component{
+	constructor(props){
+		super(props)
+		this.state = {quiz_position:1}
+	}
+	render(){
+		return (
+			<div>
+				<div className="quizQuestion">{quizData.quiz_questions[0].quiz_question}</div>
+			</div>
+		)
+	}
+}
+
+export default Quiz;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import Quiz from './Quiz.js';
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div />
-    )
-  }
+	render(){
+		return(
+			<Quiz/>
+		);
+	}
+  
 }
 
-export default App
+export default App;
